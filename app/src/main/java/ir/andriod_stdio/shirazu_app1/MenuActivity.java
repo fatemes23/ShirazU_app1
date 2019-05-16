dc0cf046693567ddcf2efbaf57128899aeaf7b0a
Sadafx <sadaf98x@gmail.com>
2019-05-16 18:43:43 +0430

    menu modification #1

--- app/src/main/java/ir/andriod_stdio/shirazu_app1/MenuActivity.java
+++ app/src/main/java/ir/andriod_stdio/shirazu_app1/MenuActivity.java
@@ -1,5 +1,9 @@
 package ir.andriod_stdio.shirazu_app1;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -7,8 +11,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private ImageView shz_logo;
+    private LinearLayout buttons;
 
     private Button news;
     private Button announcement;
@@ -18,13 +27,46 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        //_______________________
+
+        findviews();
+        logoAnimation();
+        onClick();
+
+    }
+
+    public void findviews(){
+        shz_logo = (ImageView) findViewById(R.id.shz_img);
+        buttons = (LinearLayout) findViewById(R.id.buttons);
         news = (Button) findViewById(R.id.news);
         announcement = (Button) findViewById(R.id.announcement);
         email = (Button) findViewById(R.id.email);
+    }
 
-        //_________________
+    public void logoAnimation(){
+        ObjectAnimator animatorXscale = ObjectAnimator.ofFloat(shz_logo, "ScaleX", 1f, 1.5f, 1f);
+        ObjectAnimator animatorYscale= ObjectAnimator.ofFloat(shz_logo, "ScaleY", 1f, 1.5f, 1f);
+        ObjectAnimator animatorYtranslate= ObjectAnimator.ofFloat(shz_logo, "translationY", -400f);
+        animatorXscale.setDuration(3000);
+        animatorYscale.setDuration(3000);
+        animatorYtranslate.setDuration(3000);
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(animatorXscale,animatorYscale,animatorYtranslate);
+        animatorSet.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                buttons.setVisibility(View.VISIBLE);
+            }
 
+            @Override
+            public void onAnimationStart(Animator animation) {
+                super.onAnimationStart(animation);
+            }
+        });
+        animatorSet.start();
+    }
+
+    public void onClick(){
         news.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,11 +98,6 @@ public class MenuActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
-
     }
-
-
 
 }
